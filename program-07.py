51e692bca90c97ed2c9af3baa175a26b17ed5471
Ankit Ghanghas <aghangha@desktop.scholar.rcac.purdue.edu>
2020-03-20 15:00:07 -0400

    added comments to script

--- program-07.py
+++ program-07.py
@@ -1,24 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on March 5 2020
+Creator: Ankit Ghanghas
+
+program_07.py
+
+This script reads csv file as pandas dataframe and produces basic plots for visual analysis using Matplotlib.
+
+The output of this script is 8 png files in the root directory namely histogram.png, kde.png, latlong.png, cdf.png, variationwithdepth.pnd, normal.png and gumbelqq.png
+
+"""
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
 
-df_quakes=pd.read_csv("all_month.csv")
+#so far these import important modueles required for this script
+df_quakes=pd.read_csv("all_month.csv") # imports all_month.csv present in the present directory as df_quakes
+#Plot the histogram
 binwidth=1
-plt.figure()
-plt.hist(df_quakes[np.isfinite(df_quakes['mag'])].mag.values,bins=range(0,10+binwidth,binwidth))
-plt.title('Histogram of Earthquake magnitude (binwidth ='+ str(binwidth)+')')
-plt.xlabel('Earthquake Magnitude')
-plt.ylabel('Number of events')
-plt.savefig('histogram.png')
-plt.close()
+plt.figure() # generates a new blank figure
+plt.hist(df_quakes[np.isfinite(df_quakes['mag'])].mag.values,bins=range(0,10+binwidth,binwidth)) #first finds points where the magnitude is finite then only for those indices makes a histogram of magnitude of bin width 1.
+plt.title('Histogram of Earthquake magnitude (binwidth ='+ str(binwidth)+')')# adds title to the plot.
+plt.xlabel('Earthquake Magnitude')# adds label to the x axis
+plt.ylabel('Number of events')# adds label to the y-axis
+plt.savefig('histogram.png') #saves the figure as specified name
+plt.close() # closes the figure
+
 #plot kde
-df_quakes[np.isfinite(df_quakes['mag'])].mag.plot.kde(title="KDE of Magnitude")
+df_quakes[np.isfinite(df_quakes['mag'])].mag.plot.kde(title="KDE of Magnitude")#uses inbulit kde method of pandas module to cerate and plot it.
 plt.savefig('kde.png')
+
+#plot the scatter of longitude and lattitude
 plt.figure()
-plt.scatter(df_quakes[np.isfinite(df_quakes['mag'])].longitude, df_quakes[np.isfinite(df_quakes['mag'])].latitude)
+plt.scatter(df_quakes[np.isfinite(df_quakes['mag'])].longitude, df_quakes[np.isfinite(df_quakes['mag'])].latitude) # scatter plot with longitude as xaxis and latitude as yaxis.
 plt.title("Earthquakes across the Lat and Long")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
@@ -34,6 +51,7 @@ plt.ylabel("Cumulative Frequency")
 plt.savefig('cdf.png')
 plt.close()
 
+# plotting a scatter plot of magnitude against depth for all cases where we have definite magnitude
 plt.figure()
 plt.scatter(df_quakes[np.isfinite(df_quakes['mag'])].mag,df_quakes[np.isfinite(df_quakes['mag'])].depth)
 plt.title("Earthquake magnitude variation with depth")
@@ -42,19 +60,24 @@ plt.ylabel("Depth(km)")
 plt.savefig('variationwithdepth.png')
 plt.close()
 
+# plots qq plot of magnitude assuming gumbel distribution. We use probplt method in stats for plotting qq-plot
 plt.figure()
 stats.probplot(df_quakes[np.isfinite(df_quakes['mag'])].mag.values,dist=stats.gumbel_r,plot=plt)
 plt.title("Gumbel QQ-Plot")
 plt.savefig('gumbelqq.png')
 plt.close()
+
+# qq-plot assuming normal distribution.
 plt.figure()
 stats.probplot(df_quakes[np.isfinite(df_quakes['mag'])].mag.values,dist=stats.norm,plot=plt)
 plt.title("Normal QQ-Plot")
 plt.savefig('normal.png')
 plt.close()
-plt.figure()
-stats.probplot(df_quakes[np.isfinite(df_quakes['mag'])].mag.values,dist=stats.genpareto(-0.75),plot=plt)
-plt.title("Pareto Distribution QQ-Plot")
-plt.savefig('pareto.png')
-plt.close()
+
+#qq-plot assuming pareto distribution.
+#plt.figure()
+#stats.probplot(df_quakes[np.isfinite(df_quakes['mag'])].mag.values,dist=stats.genpareto(-0.75),plot=plt)
+#plt.title("Pareto Distribution QQ-Plot")
+#plt.savefig('pareto.png')
+#plt.close()
 
